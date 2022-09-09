544981bc93faf6e0a29889eedea2d5b507a9f994
callistus25 <callistus.obidiozor@gmail.com>
2022-09-08 18:43:20 -0700

    Hello world update

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#!/bin/bash
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: prints words as described below,"
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
