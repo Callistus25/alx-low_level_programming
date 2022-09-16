b2b6ba77c0040a7978263fb4eba0508b9474661d
callistus25 <callistus.obidiozor@gmail.com>
2022-09-15 20:17:16 -0700

    Update Header file

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -2,20 +2,16 @@
 #define MAIN_H
 
 int _putchar(char);
-int _isupper(int);
-int _isdigit(int);
-int mul(int , int);
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
 void print_numbers(void);
 void print_most_numbers(void);
 void more_numbers(void);
-void print_line(int);
-void print_diagonal(int);
-void print_square(int);
-void print_triangle(int);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
 void print_number(int);
 
 #endif /* MAIN_H */
-Footer
-© 2022 GitHub, Inc.
-Footer navigation
-Terms
