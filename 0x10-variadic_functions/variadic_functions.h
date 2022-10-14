a7ba3463e24ad4e7c3c492d81cf0484eef317aff
Callistus25 <callistus.obidiozor@gmail.com>
2022-10-13 17:09:42 -0700

    variadic func

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,10 +1,24 @@
-#ifndef VARIADIC_FUNCTIONS_H_
-#define VARIADIC_FUNCTIONS_H_
-
-int _putchar(char c);
+#ifndef _VARIADIC_FUNCTIONS_H_
+#define _VARIADIC_FUNCTIONS_H_
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * struct format - finds the format of info
+ * @flag: flag given to the format struct.
+ * @f: call back function for the struct.
+ */
+typedef struct format
+{
+	char *flag;
+	void (*f)(va_list);
+} formatter;
 int sum_them_all(const unsigned int n, ...);
-void print_numbers(const char *seperator, const unsigned int n, ...);
-void print_strings(const char *seperator, const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
-
-#endif /* VARIADIC_FUNCTIONS_H_ */
+void print_int(va_list);
+void print_char(va_list);
+void print_float(va_list);
+void print_string(va_list);
+#endif
